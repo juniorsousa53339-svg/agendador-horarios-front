d115db0c5d0bb865d446493bea3042ceb7c94e02
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-04-17 14:31:55 -0300

    new

---
+++ src/app/features/agendamento-publico/components/servico-card/servico-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servico-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './servico-card.component.html',
+  styleUrl: './servico-card.component.scss'
+})
+export class ServicoCardComponent {
+
+}
---
+++ src/app/features/agendamento-publico/pages/agendar-wizard/agendar-wizard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agendar-wizard',
+  standalone: true,
+  imports: [],
+  templateUrl: './agendar-wizard.component.html',
+  styleUrl: './agendar-wizard.component.scss'
+})
+export class AgendarWizardComponent {
+
+}
